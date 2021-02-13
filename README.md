1798fb750a15d283710ca0aedfeaa15c3df29d65
Marlrus <36910211+Marlrus@users.noreply.github.com>
2021-02-13 15:19:38 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Frontend Repo
+
+I will use Create React App using TypeScript to develop the frontend. To manage state, I will use redux along with redux-thunk for async actions.
---
+++ src/StyleConstants.ts
@@ -0,0 +1,18 @@
+// Colors
+
+// Items
+export const lucaBlue = "#3843D0";
+export const lucaDarkBlue = "#212B80";
+export const lucaRed = "#F96654";
+export const lucaYellow = "#FFD704";
+export const lucaPink = "#FC9CFE";
+
+// Background
+export const white = "#FFFFFF";
+export const lightGrayBg = "#E5E5E5";
+export const medGrayBg = "#F1F2F8";
+export const medBlueBg = "#EDEEFF";
+
+// Icon
+export const disabledIconGrayLight = "#D9DAE3";
+export const disabledIconGray = "#9A9CB5";
